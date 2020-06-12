c730ed1df67814ef9afcd11e503e4d8dba603e0b
ChrisSorrell <52987387+ChrisSorrell@users.noreply.github.com>
2020-06-11 21:51:09 -0400

    Update UniswapV2Router02.sol

--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.6.6;
+pragma solidity ^0.6.0;
 
 import 'https://github.com/Uniswap/uniswap-v2-core/blob/master/contracts/interfaces/IUniswapV2Factory.sol';
 import 'https://github.com/Uniswap/uniswap-lib/blob/master/contracts/libraries/TransferHelper.sol';
